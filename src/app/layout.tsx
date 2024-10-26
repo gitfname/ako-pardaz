da4c43c377b3a1b606c1e0d70bd4988db80d77d5
NewAma <newamailnam@gmail.com>
2024-10-26 12:26:23 +0330

    fixed a thing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { SideBar } from "./components";
-import TopNavigationBar from "./components/TopNavigationBar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
